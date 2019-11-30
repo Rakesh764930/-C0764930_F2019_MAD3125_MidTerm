634b3d751368782d9420485258fd47e0304a9e4b
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-11-30 13:35:39 -0500

    workin on cpp

--- app/src/main/java/com/example/taxcalculation/CRACustomer.java
+++ app/src/main/java/com/example/taxcalculation/CRACustomer.java
@@ -9,6 +9,12 @@ public class CRACustomer  {
 
 
 
+    public double cppAmount(double gross_income){
+        double gi=gross_income;
+        double cpp_slab=57400.00;
+        double cpp_rate=5.10;
+        double actual_cpp=0.0;
+
 
 
 }
