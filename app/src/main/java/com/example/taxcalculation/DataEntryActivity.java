ea98e7fda4a3e1e154b58a94b8200393e3de7eef
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-11-29 20:44:29 -0500

    date picker dialog

--- app/src/main/java/com/example/taxcalculation/DataEntryActivity.java
+++ app/src/main/java/com/example/taxcalculation/DataEntryActivity.java
@@ -2,13 +2,25 @@ package com.example.taxcalculation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DataEntryActivity  extends AppCompatActivity {
 
+    Button btnDate;
+    DatePickerDialog datePickerDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dataentry);
+        btnDate=findViewById(R.id.btnDate);
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
