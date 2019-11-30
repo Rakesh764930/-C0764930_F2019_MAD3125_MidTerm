5d1be5e47faab8d7ada7b766f58cee753eb852e8
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-11-30 13:37:16 -0500

    working on rrsp

--- app/src/main/java/com/example/taxcalculation/CRACustomer.java
+++ app/src/main/java/com/example/taxcalculation/CRACustomer.java
@@ -23,6 +23,21 @@ public class CRACustomer  {
         }
             return actual_cpp;
     }
+    public double cppAmount(double gross_income){
+        double gi=gross_income;
+        double cpp_slab=57400.00;
+        double cpp_rate=5.10;
+        double actual_cpp=0.0;
+        if(gi>=cpp_slab)
+        {
+            actual_cpp=(cpp_slab*cpp_rate)/100;
+        }
+        else {
+            actual_cpp=(gi*cpp_rate)/100;
+        }
+        return actual_cpp;
+    }
+
 
 
 }
