3f835869e6eeb10b25d00b16bf87d3910cf7fd4e
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-11-29 20:56:54 -0500

    text view added for displaying date

--- app/src/main/java/com/example/taxcalculation/DataEntryActivity.java
+++ app/src/main/java/com/example/taxcalculation/DataEntryActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Calendar;
+
 public class DataEntryActivity  extends AppCompatActivity {
 
     Button btnDate;
@@ -19,6 +21,12 @@ public class DataEntryActivity  extends AppCompatActivity {
         btnDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Calendar calendar=Calendar.getInstance();
+                 int year=calendar.get(Calendar.YEAR);
+                int month=calendar.get(Calendar.MONTH);
+                int day=calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog=new DatePickerDialog(DataEntryActivity.this,android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth,)
 
             }
         });
