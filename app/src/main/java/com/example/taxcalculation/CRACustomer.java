cc7f76922657d254767e0d4cb71ff7f8695360f5
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-11-30 13:52:59 -0500

    variables created

--- app/src/main/java/com/example/taxcalculation/CRACustomer.java
+++ app/src/main/java/com/example/taxcalculation/CRACustomer.java
@@ -1,17 +1,21 @@
 package com.example.taxcalculation;
 
+import android.app.AlertDialog;
+import android.widget.Toast;
+
 public class CRACustomer  {
 
     int sin_number;
     String first_name;
     String  last_name;
     String full_name=last_name.toUpperCase()+","+first_name;
-
-
+    double grossIncome;
+    double rrsp_contri;
+double EI;
 
     public double cppAmount(double gross_income){
         double gi=gross_income;
-        double cpp_slab=57400.00;
+        double cpp_slab=57_400.00;
         double cpp_rate=5.10;
         double actual_cpp=0.0;
         if(gi>=cpp_slab)
@@ -23,20 +27,11 @@ public class CRACustomer  {
         }
             return actual_cpp;
     }
-    public double cppAmount(double gross_income){
+    public double rrspAmount(double gross_income){
         double gi=gross_income;
-        double cpp_slab=57400.00;
-        double cpp_rate=5.10;
-        double actual_cpp=0.0;
-        if(gi>=cpp_slab)
-        {
-            actual_cpp=(cpp_slab*cpp_rate)/100;
-        }
-        else {
-            actual_cpp=(gi*cpp_rate)/100;
-        }
-        return actual_cpp;
-    }
+        double rrsp_perc=18.00;
+        double actual_rrsp=(gi*rrsp_perc)/100;
+   
 
 
 
