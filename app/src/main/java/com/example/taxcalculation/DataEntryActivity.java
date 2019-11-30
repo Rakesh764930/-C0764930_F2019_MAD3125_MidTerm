fee90a1635f19f5aaaa3514b1b2392f6345e6514
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-11-29 21:25:23 -0500

    date picker dialog object

--- app/src/main/java/com/example/taxcalculation/DataEntryActivity.java
+++ app/src/main/java/com/example/taxcalculation/DataEntryActivity.java
@@ -1,34 +1,34 @@
 package com.example.taxcalculation;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class DataEntryActivity  extends AppCompatActivity {
 
-    Button btnDate;
+    final Calendar calendar = Calendar.getInstance();
+    TextView txtDate;
     DatePickerDialog datePickerDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dataentry);
-        btnDate=findViewById(R.id.btnDate);
-        btnDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar calendar=Calendar.getInstance();
-                 int year=calendar.get(Calendar.YEAR);
-                int month=calendar.get(Calendar.MONTH);
-                int day=calendar.get(Calendar.DAY_OF_MONTH);
 
-                DatePickerDialog dialog=new DatePickerDialog(DataEntryActivity.this,android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth,)
 
-            }
-        });
-    }
-}
+
+
+
+
+        txtDate= findViewById(R.id.txtDate);
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
