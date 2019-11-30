ae43dce8a660857b3d7dd322e6f241f128c82264
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-11-29 19:38:32 -0500

    activity created

--- app/src/main/java/com/example/taxcalculation/DataEntryActivity.java
+++ app/src/main/java/com/example/taxcalculation/DataEntryActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DataEntryActivity extends AppCompatActivity {
+public class DataEntryActivity  extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
