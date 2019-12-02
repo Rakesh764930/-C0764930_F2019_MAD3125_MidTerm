b9d0cd2dbf8c09ec067693badff316d3ae5182de
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-12-02 17:33:44 -0500

    parcelable implemented

--- app/src/main/java/com/example/taxcalculation/DataDisplayActivity.java
+++ app/src/main/java/com/example/taxcalculation/DataDisplayActivity.java
@@ -21,10 +21,10 @@ public class DataDisplayActivity extends AppCompatActivity {
     TextView txtgenDer;
     TextView txtgross_income,
             txttaxDate, txtfederal_Tax, txtprovincial_Tax, lblcpp,
-            lblEmp_Insurance, lblRRSPcontributed, lblCfRRSP,
-            lblTaxableIncome, lblTaxPaid;
-    double cpp = 0, ei = 0;  double rrsp = 0, rrspCf = 0, taxableIncome, federalTax,
-            provincialTax, totalTaxPaid;
+            txtEmpInsurance, txtRRSPcontri, txtCfRRSP,
+            txtTaxableIncome, lblTaxPaid;
+    double cpp = 0, ei = 0;  double rrsp = 0, rrspCf = 0, taxableIncome, fedTax,
+            proTax, totalTaxPaid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,11 +33,11 @@ public class DataDisplayActivity extends AppCompatActivity {
         txtfull_Name = findViewById(R.id.txtfull_Name);
         txtgenDer =   findViewById(R.id.txtGender);
         txtgross_income = findViewById(R.id.txtgrossIncome);
-        lblRRSPcontributed = findViewById(R.id.txt_D_RRSPContri);
+        txtRRSPcontri = findViewById(R.id.txt_D_RRSPContri);
         lblcpp = findViewById(R.id.txtCpp);
-        lblEmp_Insurance = findViewById(R.id.txt_D_empInsurance);
-        lblCfRRSP = findViewById(R.id.txt_D_cfRRSP);
-        lblTaxableIncome = findViewById(R.id.txt_D_taxableIncome);
+        txtEmpInsurance = findViewById(R.id.txt_D_empInsurance);
+        txtCfRRSP = findViewById(R.id.txt_D_cfRRSP);
+        txtTaxableIncome = findViewById(R.id.txt_D_taxableIncome);
         txtfederal_Tax = findViewById(R.id.txt_D_federalTax);
         txtprovincial_Tax = findViewById(R.id.txt_D_provincialTax);
         lblTaxPaid = findViewById(R.id.txt_D_taxPayed);
@@ -47,69 +47,57 @@ public class DataDisplayActivity extends AppCompatActivity {
         Intent mIntent = getIntent();
         CRACustomer customer = mIntent.getParcelableExtra("CRACustomer");
 
-        txtsin.setText(" SIN: \t" + customer.getSin_number());
-        txtfull_Name.setText(" FULL NAME: \t" + customer.getFull_name());
+        txtsin.setText("\tSIN: \t" + customer.getSin_number());
+        txtfull_Name.setText("\tFULL NAME: \t" + customer.getFull_name());
         txtgenDer.setText(" GENDER: \t" + customer.getGender());
-        txtgross_income.setText(" GROSS INCOME: \t" + customer.getGrossIncome());
-        lblRRSPcontributed.setText("RRSP Contributed: \t" + customer.getRrsp_contri());
+        txtgross_income.setText("\tGROSS INCOME: \t" + customer.getGrossIncome());
+        txtRRSPcontri.setText("\tRRSP Contributed: \t" + customer.getRrsp_contri());
 
         // calculate  cpp
         double grossIncome = customer.getGrossIncome();
         if(grossIncome > 57400.00){
-            cpp = (57400.00 * 0.051); //5.10%
+            cpp = (57400.00 * 0.051); //perc= 5.10%
         } else {
             cpp = (grossIncome * 0.051);
         }
-        lblcpp.setText("CPP COntribution in Year:\t" + cpp);
+        lblcpp.setText("\tCPP COntribution in Year:\t" + cpp);
         // calculate employement insurance
         if(grossIncome > 53100){
-            ei = (53100 * 0.0162); //1.62%
+            ei = (53100 * 0.0162); //perc 1.62%
         }else{
             ei = (grossIncome * (1.62/100));
         }
-        lblEmp_Insurance.setText("Employeement Insurance: \t" + ei);
+        txtEmpInsurance.setText("\tEmployeement Insurance: \t" + ei);
         // calculate RRSP
         rrsp = customer.getRrsp_contri();
-        double maxRRSP = (grossIncome * 0.18); //18%
+        double maxRRSP = (grossIncome * 0.18); //perc= 18%
         if(rrsp > maxRRSP ){
+            Toast.makeText(this,"you mave have to face a penalty,amount exceeding",Toast.LENGTH_SHORT).show();
             rrspCf = rrsp - maxRRSP;
             rrsp = maxRRSP;
         }else{
             rrsp = rrsp;
         }
-        lblCfRRSP.setText("RRSP Carry forward: \t"+ rrspCf);
+        txtCfRRSP.setText("\tRRSP Carried forward: \t"+ rrspCf);
         //taxable income
         taxableIncome = grossIncome - (cpp + ei + rrsp);
 
-        lblTaxableIncome.setText("Taxable income:\t" + (double) taxableIncome);
+        txtTaxableIncome.setText("\tTotal Taxable income:\t" + (double) taxableIncome);
         //federal tax
-        double calFederal = calcFedralTax();
-        txtfederal_Tax.setText("Federal Tax: \t" + calFederal);
+        double fed_tax = federalTax();
+        txtfederal_Tax.setText("\tFederal Tax: \t" + fed_tax);
         // Provincial Tax
-        double calProvincial = calcProvincialTax();
-        txtprovincial_Tax.setText("Provincial Tax:\t" + calProvincial);
+        double pro_tax = provincialTax();
+        txtprovincial_Tax.setText("\tProvincial Tax:\t" + pro_tax);
         // total tax paid
-        double taxpaid = calTaxPaid();
-        lblTaxPaid.setText("Total tax Paid:\t" + taxpaid);
+        double taxpaid = TaxPaid();
+        lblTaxPaid.setText("\tTotal tax Paid:\t" + taxpaid);
 
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case android.R.id.home:
-                finish();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return true;
-    }
     public double calcCpp(){
-        // calculate  cpp
+        // calculating  cpp
         if(customer.getGrossIncome() > 57400.00){
             cpp = (57400.00 * 0.051);
         } else {
@@ -117,67 +105,69 @@ public class DataDisplayActivity extends AppCompatActivity {
         }
         return cpp;
     }
-    public double calcFedralTax(){
-        //calculate federal tax
+    //calculating federal tax
+    public double federalTax(){
+
         double temp = taxableIncome ;
         if(temp <= 12069.00){
-            federalTax = 0;//0%
+            fedTax = 0;//0%
             temp = taxableIncome - 12069.00;
         }
         if(temp >= 12069.01){
-            federalTax = (temp * 0.15);//15%
+            fedTax = (temp * 0.15);//perc= 15%
             temp = temp - 35561;
         }
         if(temp >= 47630.01){
-            federalTax = (temp * 0.205); //20.50%
+            fedTax = (temp * 0.205); //perc= 20.50%
             temp = temp - 47628.99;
         }
         if(temp >= 95259.01){
-            federalTax = (temp * 0.26); //26%
+            fedTax = (temp * 0.26); //perc= 26%
             temp = temp - 52407.99;
         }
         if (temp >= 147667.01){
-            federalTax = (temp * 0.29);//29%
+            fedTax = (temp * 0.29);//perc= 29%
             temp = temp - 62703.99;
         }
         if(temp >= 210371.01){
-            federalTax = (temp * 0.33);//33%
+            fedTax = (temp * 0.33);//perc= 33%
             //temp = temp - federalTax;
         }
-        return federalTax;
+        return fedTax;
     }
-    public  double calcProvincialTax(){
-        //calculate provincial tax
+
+    //calculate provincial tax
+    public  double provincialTax(){
         double temp = taxableIncome ;
 
         if(temp <= 10582.00){
-            provincialTax = 0;
+            proTax = 0;
             temp = taxableIncome - 10582.00;
         }
         if(temp >= 10582.01){
-            provincialTax = (temp * 0.0505); //5.05%
+            proTax = (temp * 0.0505); // perc= 5.05%
             temp = temp - 33323.99;
         }
         if(temp >= 43906.01){
-            provincialTax = (temp * 0.0915); //9.15%
+            proTax = (temp * 0.0915); //perc= 9.15%
             temp = temp - 43906.99;
         }
         if(temp >= 87813.01){
-            provincialTax = (temp * 0.1116); //11.16%
+            proTax = (temp * 0.1116); //perc= 11.16%
             temp = temp - 62187.99;
         }
         if (temp >= 150000.01){
-            provincialTax = (temp * 0.1216);//12.16%
+            proTax = (temp * 0.1216);//perc= 12.16%
             temp = temp - 69999.99;
         }
         if(temp >= 220000.01){
-            provincialTax = (temp * 0.1316);//13.16%
+            proTax = (temp * 0.1316);//perc= 13.16%
 
         }
-        return provincialTax;
+        return proTax;
     }
-    public  double calTaxPaid(){
-        return totalTaxPaid;
+    public  double TaxPaid(){
+        return totalTaxPaid=fedTax+proTax;
     }
 
 }
